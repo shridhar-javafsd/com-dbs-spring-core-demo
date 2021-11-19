560376f63f8e5d2b238db0818b3085e6e9cc47ad
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-19 15:45:48 +0530

    Update HibernateDemo.java

--- src/main/java/com/dbs/demo/hb/HibernateDemo.java
+++ src/main/java/com/dbs/demo/hb/HibernateDemo.java
@@ -1,15 +1,27 @@
 package com.dbs.demo.hb;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.dbs.demo.Employee;
+
 public class HibernateDemo {
 
 	public static void main(String[] args) {
 
 		System.out.println("Start");
-		
-		
-		
-		
 
+		Configuration config = new Configuration();
+
+		SessionFactory factory = config.configure().buildSessionFactory();
+
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Employee emp = new Employee(101, "Sonu", 45678.9);
+		session.save(emp);
+		transaction.commit();
 		System.out.println("End");
 	}
 
